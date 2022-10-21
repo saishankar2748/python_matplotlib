53709aca18d13b8c3e37974a5e509d1d9987f625
saishankar2748 <saishankar2748@gmail.com>
2022-10-21 23:29:37 +0530

    horizontal bargraph using counter module

--- VS/02_bargraph/02_horizontal_bargraph.py
+++ VS/02_bargraph/02_horizontal_bargraph.py
@@ -2,7 +2,38 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 import numpy as np
-import csv
 
-df = pd.read_csv('C:\\Users\\saish\\python_modules\\VS\\02_bargraph\\data.txt',sep = ',',index_col='Responder_id')
-print(df.head())
+df = pd.read_csv('C:\\Users\\saish\\python_modules\\VS\\02_bargraph\\data.txt',sep = ',')
+# print(df.head())
+
+lang_responses = df['LanguagesWorkedWith']
+
+language_counter = Counter()
+
+for response in lang_responses:
+    language_counter.update(response.split(';'))
+
+# print(language_counter)  # returns dictionary of counted values
+# print(language_counter.most_common(15))
+
+languages = []
+popularity = []
+
+for item in language_counter.most_common(15): # returns tuple
+    languages.append(item[0])
+    popularity.append(item[1])
+
+# print(languages)
+# print(popularity)
+
+languages.reverse()
+popularity.reverse()
+
+plt.barh(languages,popularity)
+
+plt.title("Most Popular Languages")
+# plt.ylabel("Programming Languages")
+plt.xlabel("Number of People Who Use")
+
+plt.tight_layout()
+plt.show()
