842155135eb68bcf15b7cc408d87aa0571089511
saishankar2748 <saishankar2748@gmail.com>
2022-10-24 17:43:43 +0530

    plotting live data

---
+++ VS/09_live_data/09_live_data_plotting.py
@@ -0,0 +1,30 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+def animate(i):
+    data = pd.read_csv('C:\\Users\\saish\\python_modules\\VS\\09_live_data\\data.csv')
+    x = data['x_value']
+    y1 = data['total_1']
+    y2 = data['total_2']
+
+
+    plt.cla()    # clears axis
+
+    plt.plot(x, y1, label='Channel 1')
+    plt.plot(x, y2, label='Channel 2')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+
+plt.tight_layout()
+plt.show()
+
+
